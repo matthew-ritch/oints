f5e8da29fdbe52cb12ac7a3cfc7a58e4a178c5b5
mdr <matthew.drummond.ritch@gmail.com>
2024-06-30 12:02:25 -0400

    functioning views

--- ointmanager/urls.py
+++ ointmanager/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("api/checkbalance/<str:oint_id>/<str:address>", views.checkbalance, name="checkbalance"),
+    path("api/mint_oints_to_address/<str:oint_id>/<str:address>/<int:amount>/<str:company_api_key>", views.mint_oints_to_address, name="mint_oints_to_address"),
 ]
--- ointmanager/views.py
+++ ointmanager/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 
 from ointmanager.models import Oint, User
 
-import requests
+import requests, json
 from dotenv import dotenv_values
 from web3 import Web3, AsyncWeb3
 
@@ -21,21 +21,21 @@ def index(request):
 
 #checkbalance
 
-def checkbalance(oint_id, address):
+def checkbalance(request, oint_id, address):
     oint = get_object_or_404(Oint, pk=oint_id)
-    token = w3.eth.contract(address=oint.contract_address, abi=oint.abi) # declaring the token contract
+    token = w3.eth.contract(address=oint.contract_address, abi=oint.abi_string) # declaring the token contract
     token_balance = token.functions.balanceOf(address).call() # returns int with balance, without decimals
     return JsonResponse({"oint_id":oint_id, "holder_address":address, "token_balance":token_balance})
 
 #mint oints
 
-def mint_oints_to_user(oint_id, user_id, company_api_key):
+def mint_oints_to_user(request, oint_id, user_id, amount, company_api_key):
     user = get_object_or_404(User, pk=user_id)
-    return mint_oints_to_address(oint_id, user.wallet.address, company_api_key)
+    return mint_oints_to_address(oint_id, user.wallet.address, amount, company_api_key)
 
-def mint_oints_to_address(oint_id, address, company_api_key):
+def mint_oints_to_address(request, oint_id, address, amount, company_api_key):
     oint = get_object_or_404(Oint, pk=oint_id)
-    assert oint.company.api_key == company_api_key
+    assert str(oint.company.api_key) == str(company_api_key)
 
     # Initialize the address calling the functions/signing transactions
     caller = dotenv_values(".env")['WALLET_ADDRESS']
@@ -45,12 +45,12 @@ def mint_oints_to_address(oint_id, address, company_api_key):
     contract = w3.eth.contract(address=oint.contract_address, abi=oint.abi_string)
     # call function, sign, and send transaction
     Chain_id = w3.eth.chain_id
-    call_function = contract.functions.mint(address).buildTransaction({"chainId": Chain_id, "from": caller, "nonce": nonce})
+    call_function = contract.functions.mint(address, amount * 10**18).build_transaction({"chainId": Chain_id, "from": caller, "nonce": nonce})
     signed_tx = w3.eth.account.sign_transaction(call_function, private_key=private_key)
     sent_tx = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
     # Wait for transaction receipt
     tx_receipt = w3.eth.wait_for_transaction_receipt(sent_tx)
-
-    return JsonResponse(tx_receipt)
+    print(tx_receipt)
+    return JsonResponse({"status":"success","tx":sent_tx.hex()})
 
 #use oints
